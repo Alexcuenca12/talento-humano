a673b81c96452e83fc57513fb9dca0d532633078
ForaneoBlack <foraneoblack@gmail.com>
2023-06-08 17:27:42 -0500

    CargaFamiliar
    Primera parte de la carga familiar, ademas de la integracion de bootstrap en el index

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './views/Login/LoginContext';
+import CargaFamiliar from "./views/CargaFamiliar/CargaFamiliar";
+
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
-  <Login/>
+  <CargaFamiliar/>
   );
 }
 
