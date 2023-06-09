328e5eb3485adf2191f237d20af4393a08efba18
Alexis <mateosiavig@gmail.com>
2023-06-09 07:50:19 -0500

    Revert "CargaFamiliar"
    This reverts commit a673b81c96452e83fc57513fb9dca0d532633078.

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './views/Login/LoginContext';
-import CargaFamiliar from "./views/CargaFamiliar/CargaFamiliar";
-
-import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
-  <CargaFamiliar/>
+  <Login/>
   );
 }
 
