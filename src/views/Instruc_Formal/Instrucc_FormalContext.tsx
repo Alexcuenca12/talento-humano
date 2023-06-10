4eab2d14c4eb6c28835db13b1b4840ea5dc9fc11
Alberto2208 <avallejo@libelulasoft.com>
2023-06-09 22:11:04 -0500

    ca,mbion

--- src/views/Instruc_Formal/Instrucc_FormalContext.tsx
+++ src/views/Instruc_Formal/Instrucc_FormalContext.tsx
@@ -1,1 +1,102 @@
-export {};
+import React from 'react';
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
+import '../../styles/Instruc_Formal.css'; 
+
+
+class PrimeReactForm extends React.Component {
+  constructor(props: {} | Readonly<{}>) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      phone: ''
+    };
+  }
+
+  handleChange = (event: { target: { name: any; value: any; }; }) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit = (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+    // Aquí puedes agregar la lógica para enviar los datos del formulario
+  }
+
+  render() {
+    return (
+      <div>
+        <div style={{ marginBottom: '120px' }}></div>
+  
+        <div className="centered-form">
+          <div className="title-container">
+            <div className="title-line"></div>
+            <h1 className="page-title">INSTRUCCION FORMAL</h1>
+            <div className="title-line"></div>
+          </div>
+          <form onSubmit={this.handleSubmit}>
+            <div className="form-row">
+              <label htmlFor="firstName">Nivel de Institución:</label>
+              <InputText className="small-input" id="firstName" name="firstName" onChange={this.handleChange} />
+              <label htmlFor="lastName">Titulo Obtenido:</label>
+              <InputText className="small-input" id="lastName" name="lastName" onChange={this.handleChange} />
+            </div>
+            <div className="form-row">
+              <label htmlFor="email">Institucion Educativa:</label>
+              <InputText className="small-input" id="email" name="email" onChange={this.handleChange} />
+              <label htmlFor="phone">Tiempo de Estudio:</label>
+              <InputText className="small-input" id="phone" name="phone" onChange={this.handleChange} />
+            </div>
+            <div className="form-row-buttons">
+              <Button type="submit" label="Submit" className="small-button p-button-success" style={{ background: 'black' }} />
+              <Button type="button" label="Cancel" className="small-button p-button-secondary" style={{ background: 'black' }} />
+            </div>
+          </form>
+          <div className="table-container">
+            <table className="data-table">
+              <thead>
+                <tr>
+                  <th>Nivel de Institución</th>
+                  <th>Institución Educativa</th>
+                  <th>Título Obtenido</th>
+                  <th>Tiempo de Estudio</th>
+                  <th>Valores</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Primaria</td>
+                  <td>Escuela XYZ</td>
+                  <td>Graduado en Primaria</td>
+                  <td>6 años</td>
+                  <td>Responsabilidad, Honestidad</td>
+                </tr>
+                <tr>
+                  <td>Secundaria</td>
+                  <td>Colegio ABC</td>
+                  <td>Graduado en Secundaria</td>
+                  <td>4 años</td>
+                  <td>Trabajo en equipo, Disciplina</td>
+                </tr>
+                <tr>
+                  <td>Universidad</td>
+                  <td>Universidad 123</td>
+                  <td>Título Universitario</td>
+                  <td>5 años</td>
+                  <td>Perseverancia, Liderazgo</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="additional-button-container">
+          <Button type="button" label="Additional Button" className="small-button p-button-secondary additional-button" style={{ background: 'black' }} />
+</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PrimeReactForm;
---
+++ src/views/Instruct_Formal2/Instrucc_FormalContext2.tsx
@@ -0,0 +1,184 @@
+import React from 'react';
+import { InputText } from 'primereact/inputtext';
+import { FileUpload } from 'primereact/fileupload';
+import { Button } from 'primereact/button';
+import { Calendar } from 'primereact/calendar';
+import '../../styles/Instruct_Formal2.css';
+
+class PrimeReactForm2 extends React.Component {
+  constructor(props: {} | Readonly<{}>) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      phone: '',
+      file: null,
+    };
+  }
+
+  handleChange = (event: { target: { name: any; value: any; }; }) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  handleFileUpload = (event: { target: { files: any[]; }; }) => {
+    const file = event.target.files[0];
+    this.setState({ file });
+  }
+
+  handleSubmit = (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+    // Aquí puedes agregar la lógica para enviar los datos del formulario
+  }
+
+  render() {
+    return (
+      <div>
+      <div style={{ marginBottom: '120px' }}></div>
+
+      <div className="centered-form">
+        <div className="title-container">
+          <div className="title-line"></div>
+          <h1 className="page-title">INSTRUCCION FORMAL</h1>
+          <div className="title-line"></div>
+        </div>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-row">
+            <div className="input-container">
+              <label htmlFor="level">Nivel de Institución:</label>
+              <InputText className="small-input" id="level" name="level" onChange={this.handleChange} />
+            </div>
+            <div className="input-container">
+              <label htmlFor="title">Título Obtenido:</label>
+              <InputText className="small-input" id="title" name="title" onChange={this.handleChange} />
+            </div>
+            <div className="input-container">
+  <label htmlFor="institution">Tiempo de Estudio:</label>
+  <Calendar
+    className="small-input"
+    id="institution"
+    name="institution"
+    dateFormat="dd/mm/yy"
+    showIcon
+    onChange={this.handleChange}
+  />
+</div>
+          </div>
+          <div className="form-row">
+            <div className="input-container">
+              <label htmlFor="duration">Institución Educativa:</label>
+              <InputText className="small-input" id="duration" name="duration" onChange={this.handleChange} />
+            </div>
+            <div className="input-container">
+              <label htmlFor="input6">Registro Seneciyt:</label>
+              <InputText className="small-input" id="input6" name="input6" onChange={this.handleChange} />
+            </div>
+            <div className="input-container">
+  <label htmlFor="institution">Tiempo de Estudio:</label>
+  <Calendar
+    className="small-input"
+    id="institution"
+    name="institution"
+    dateFormat="dd/mm/yy"
+    showIcon
+    onChange={this.handleChange}
+  />
+</div>
+          </div>
+          <div className="form-row">
+            <div className="input-container">
+              <label htmlFor="input8">Area de estudio:</label>
+              <InputText className="small-input" id="input8" name="input8" onChange={this.handleChange} />
+            </div>
+            <div className="input-container">
+              
+              
+            </div>
+            <div className="input-container">
+  <label htmlFor="pdf">Subir PDF:</label>
+  <FileUpload
+    className="small-input"
+    id="pdf"
+    name="pdf"
+    chooseLabel="Seleccionar"
+    mode="basic"
+    uploadLabel="Subir"
+    cancelLabel="Cancelar"
+    customUpload
+    
+    accept=".pdf"
+  />
+</div>
+          </div>
+          <div className="form-row-buttons">
+            <Button type="submit" label="Submit" className="small-button p-button-success" style={{ background: 'black' }} />
+            <Button type="button" label="Cancel" className="small-button p-button-secondary" style={{ background: 'black' }} />
+          </div>
+        </form>
+        <div className="table-container">
+          <table className="data-table">
+            <thead>
+              <tr>
+                <th>Nivel de Institución</th>
+                <th>Institución Educativa</th>
+                <th>Título Obtenido</th>
+                <th>Tiempo de Estudio</th>
+                <th>Valores</th>
+                <th>Campo 6</th>
+                <th>Campo 7</th>
+                <th>Campo 8</th>
+                <th>Campo 9</th>
+                <th>Subir PDF</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Primaria</td>
+                <td>Escuela XYZ</td>
+                <td>Graduado en Primaria</td>
+                <td>6 años</td>
+                <td>Responsabilidad, Honestidad</td>
+                <td>Dato 6</td>
+                <td>Dato 7</td>
+                <td>Dato 8</td>
+                <td>Dato 9</td>
+                <td>PDF 1</td>
+              </tr>
+              <tr>
+                <td>Secundaria</td>
+                <td>Colegio ABC</td>
+                <td>Graduado en Secundaria</td>
+                <td>4 años</td>
+                <td>Trabajo en equipo, Disciplina</td>
+                <td>Dato 6</td>
+                <td>Dato 7</td>
+                <td>Dato 8</td>
+                <td>Dato 9</td>
+                <td>PDF 2</td>
+              </tr>
+              <tr>
+                <td>Universidad</td>
+                <td>Universidad 123</td>
+                <td>Título Universitario</td>
+                <td>5 años</td>
+                <td>Perseverancia, Liderazgo</td>
+                <td>Dato 6</td>
+                <td>Dato 7</td>
+                <td>Dato 8</td>
+                <td>Dato 9</td>
+                <td>PDF 3</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className="">
+          <Button type="button" label="Additional Button" className="small-button p-button-secondary additional-button" style={{ background: 'black' }} />
+        </div>
+      </div>
+    </div>
+    );
+  }
+}
+
+export default PrimeReactForm2;
