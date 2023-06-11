efb5cf65e009f83134e21c7e4d2d2928e66e7a0e
ForaneoBlack <foraneoblack@gmail.com>
2023-06-11 13:50:32 -0500

    Cambios en capacitaciones

--- src/views/Capacitaciones/CapacitacionesContext.tsx
+++ src/views/Capacitaciones/CapacitacionesContext.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import { InputText } from 'primereact/inputtext';
+import { Calendar } from 'primereact/calendar';
+import { Dropdown } from 'primereact/dropdown';
+import { FileUpload } from 'primereact/fileupload';
 import { Button } from 'primereact/button';
 
 const Capacitaciones = () => {
@@ -15,22 +18,59 @@ const Capacitaciones = () => {
     setEmail('');
   };
 
+  const comboOptions = [
+    { label: 'Opción 1', value: 'opcion1' },
+    { label: 'Opción 2', value: 'opcion2' },
+    { label: 'Opción 3', value: 'opcion3' }
+  ];
+
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="p-fluid">
-        <div className="p-field">
-          <label htmlFor="nombre">Nombre</label>
-          <InputText id="nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} />
+      <div className="p-grid p-fluid">
+        <div className="p-col-12 p-md-4">
+          <label>Label 1</label>
+          <Dropdown options={comboOptions} placeholder="Combo Box 1" />
         </div>
-        <div className="p-field">
-          <label htmlFor="email">Email</label>
-          <InputText id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <div className="p-col-12 p-md-4">
+          <label>Label 2</label>
+          <Dropdown options={comboOptions} placeholder="Combo Box 2" />
         </div>
-        <div className="p-field">
-          <Button label="Enviar" type="submit" />
+        <div className="p-col-12 p-md-4">
+          <label>Label 3</label>
+          <Dropdown options={comboOptions} placeholder="Combo Box 3" />
+        </div>
+
+        <div className="p-col-12 p-md-4">
+          <label>Label 4</label>
+          <Calendar placeholder="Fecha 1" showIcon={true} />
+        </div>
+        <div className="p-col-12 p-md-4">
+          <label>Label 5</label>
+          <Calendar placeholder="Fecha 2" showIcon={true} />
+        </div>
+        <div className="p-col-12 p-md-4">
+          <label>Label 6</label>
+          <InputText type="number" placeholder="Número" />
+        </div>
+
+        <div className="p-col-12 p-md-4">
+          <label>Label 7</label>
+          <Dropdown options={comboOptions} placeholder="Combo Box 4" />
+        </div>
+        <div className="p-col-12 p-md-4"></div>
+        <div className="p-col-12 p-md-4"></div>
+
+        <div className="p-col-12">
+          <label>Label 8</label>
+          <FileUpload name="pdf" chooseLabel="Seleccionar archivo" />
+        </div>
+
+        <div className="p-col-12 p-md-6">
+          <Button label="Agregar" />
+        </div>
+        <div className="p-col-12 p-md-6">
+          <Button label="Cancelar" />
         </div>
       </div>
-    </form>
   );
 };
 
