f8ac4dea798fb435db4ea377c4baa54e66e5bf89
Fausto <faustocamp545@gmail.com>
2023-06-12 23:38:19 -0500

    primeflex import

--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,9 @@ import "primereact/resources/primereact.min.css";
 //icons
 import "primeicons/primeicons.css";
 
+// layout
+import "/node_modules/primeflex/primeflex.css"
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
