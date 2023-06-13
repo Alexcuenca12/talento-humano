aa14d2eb137e7d1c0d5efc9c6c55f31fc10b0208
Alberto2208 <avallejo@libelulasoft.com>
2023-06-12 20:12:14 -0500

    instruccion formal Vistas

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import { AppRouter } from "./router/AppRouter";
 
 import { authReducer } from "./reducers/authReducer";
 import { AuthContext } from "./reducers/AuthContext";
+import "primereact/resources/themes/lara-light-indigo/theme.css"; 
+import "primereact/resources/primereact.min.css"; 
+
 
 const init = () => {
   let sessionUser: any = sessionStorage.getItem("user");
