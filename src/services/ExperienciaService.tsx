c0b08ff6c07653fff386ca815e22b628a268d611
Fausto <faustocamp545@gmail.com>
2023-07-12 23:08:54 -0500

    Experiencia Service class

--- src/services/ExperienciaService.tsx
+++ src/services/ExperienciaService.tsx
@@ -1,1 +1,47 @@
-export {};
+import axios, {AxiosInstance} from "axios";
+import {IExperiencia} from "../interfaces/Primary/IExperiencia";
+
+const API_BASE_URL = 'http://localhost:8080/api/experiencia'
+
+export class ExperienciaService {
+
+    private api: AxiosInstance;
+
+    constructor() {
+        this.api = axios.create({
+            baseURL: API_BASE_URL
+        });
+    }
+
+    getAllItems() {
+        return this.api.get('/read').then(response => response.data)
+            .catch(error => {
+                throw error
+            })
+    }
+
+    createItem(item: IExperiencia) {
+        return this.api.post('/create', item).then(response => response.data)
+            .catch(error => {
+                throw error
+            })
+    }
+
+    updateItem(itemId: number, item: IExperiencia) {
+        return this.api.put(`/update/${itemId}`, item)
+            .then(response => response.data)
+            .catch(error => {
+                throw error
+            })
+    }
+
+    deleteItem(itemId: number){
+        return this.api.delete(`/delete/${itemId}`)
+            .then(response => response.data)
+            .catch(error => {
+                throw error
+            })
+    }
+
+
+}
