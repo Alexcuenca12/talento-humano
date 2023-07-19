2fc47133f717584b724192a6fba4104a3220badf
ForaneoBlack <foraneoblack@gmail.com>
2023-07-19 18:39:34 -0500

    Carga Familiar
    Actualizacion del view, service, interface

--- src/services/CargaFamiliarService.tsx
+++ src/services/CargaFamiliarService.tsx
@@ -1,24 +1,36 @@
-import axios from "axios";
+import axios, {AxiosInstance} from "axios";
 import {ICargaFamiliar} from "../interfaces/Primary/ICargaFamiliar";
 
+const API_BASE_URL = 'http://localhost:8080/api/CargaFamiliar'
 export class CargaFamiliarService {
-    baseUrl: string = "http://localhost:8080/api/CargaFamiliar/";
+    private api: AxiosInstance;
 
+    constructor() {
+        this.api = axios.create({
+            baseURL: API_BASE_URL
+        });
+    }
     getAll() {
         //Método para listar todas los Usuarios
-        return axios.get<ICargaFamiliar[]>(this.baseUrl + "read").then((res) => res.data);
+        return this.api.get( "/read").then((res) => res.data);
     }
 
     saveCarga(cargaFamiliar: ICargaFamiliar) {
-        return axios.post(this.baseUrl + "create", cargaFamiliar).then((res) => res.data);
+        return this.api.post( "/create", cargaFamiliar).then((res) => res.data)
+            .catch(error => {
+            throw error
+        })
     }
 
     updateCarga(id: number, cargaFamiliar: ICargaFamiliar) {
-        return axios.put<ICargaFamiliar>(this.baseUrl + `update/${id}`, cargaFamiliar).then((res) => res.data);
+        return this.api.put(`/update/${id}`, cargaFamiliar).then((res) => res.data)
+            .catch(error => {
+                throw error
+            })
     }
 
     deleteCarga(id: number) {
-        return axios.delete(this.baseUrl + `delete/${id}`).then((res) => res.data);
+        return this.api.delete( `/delete/${id}`).then((res) => res.data);
     }
 }
 
