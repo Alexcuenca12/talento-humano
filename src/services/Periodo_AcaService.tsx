01332463fe0caad09de603ecaf273d4b065b878f
Fausto <faustocamp545@gmail.com>
2023-08-11 19:19:27 -0500

    Periodo Academico Service

--- src/services/Periodo_AcaService.tsx
+++ src/services/Periodo_AcaService.tsx
@@ -1,1 +1,23 @@
-export {};
+import axios, {AxiosInstance} from "axios";
+
+const API_BASE_URL = 'http://localhost:8080/api/periodoacademico';
+
+class Periodo_AcaService{
+    private api: AxiosInstance;
+
+    constructor() {
+        this.api = axios.create({
+            baseURL: API_BASE_URL
+        });
+    }
+
+    getAllItems(){
+        return this.api.get('/read').then(response => response.data)
+            .catch(error=>{
+                throw error
+            })
+    }
+}
+
+const academicPeriodService = new Periodo_AcaService();
+export default academicPeriodService;
