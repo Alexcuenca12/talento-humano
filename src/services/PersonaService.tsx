53ae9df8acac20755464254b99872dfd415def8b
Fausto <faustocamp545@gmail.com>
2023-08-18 23:23:16 -0500

    Match to combined endpoint Person controller

--- src/services/PersonaService.tsx
+++ src/services/PersonaService.tsx
@@ -1,1 +1,24 @@
-export {};
+import axios, {AxiosInstance} from "axios";
+import {IResumen} from "../interfaces/Primary/IResumen";
+
+const API_BASE_URL = 'http://localhost:8080/api/persona';
+
+class PersonaService {
+    private api: AxiosInstance;
+
+    constructor() {
+        this.api = axios.create({
+            baseURL: API_BASE_URL
+        });
+    }
+
+    getSummary(id: number) {
+        return this.api.get(`/combined/${id}`).then(response => response.data as IResumen)
+            .catch(error => {
+                throw error
+            })
+    }
+}
+
+const personaService = new PersonaService();
+export default personaService;
